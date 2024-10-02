c29481f238e5eb7f6e8c6cced79d99b2d7998a84
unknown <rafif.purnomo@gmail.com>
2024-10-02 21:20:17 +0700

    Add About Us

--- src/utils/daftarPabrik.js
+++ src/utils/daftarPabrik.js
@@ -1,71 +1,75 @@
-// const data = [
-  {
-    id: 1,
-    img: "../../assets/img/Logo_AHM.png",
-    namaPerusahaan: "test1",
-    jumlahPekerjaan: 10,
+const dataDaftarPabrik = [
+    {
+      id: 1,
+      img: 'src/assets/img/Logo_AHM.png',
+      namaPerusahaan: "test1",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 2,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test2",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 2,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test2",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 3,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test3",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 3,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test3",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 4,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test4",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 4,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test4",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 5,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test5",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 5,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test5",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 6,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test6",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 6,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test6",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 7,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test7",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 7,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test7",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 8,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test8",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 8,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test8",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 9,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test9",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 9,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test9",
+      jumlahPekerjaan: 10,
 
-  },
-  {
-    id: 10,
-    img: '../../assets/img/Logo_AHM.png',
-    namaPerusahaan: "test10",
-    jumlahPekerjaan: 10,
+    },
+    {
+      id: 10,
+      img: '../assets/img/Logo_AHM.png',
+      namaPerusahaan: "test10",
+      jumlahPekerjaan: 10,
 
-  },
+    },
+    
+  ];
+  
+  export default dataDaftarPabrik;
