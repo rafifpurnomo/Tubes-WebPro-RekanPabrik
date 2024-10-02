52b77b56dd1f5f0cb220999684da04ee77252324
unknown <rafif.purnomo@gmail.com>
2024-10-02 22:29:17 +0700

    Add contact us & update navbar

---
+++ src/home-before-login/contact-us/contactUs.js
@@ -0,0 +1,24 @@
+window.onscroll = function () {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    const navbar = document.getElementById("navbar");
+  
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 90) {
+      navbar.style.backgroundColor = "rgba(255, 255, 255, 0.28);";
+      //navbar.style.backgroundColor = "rgba(4, 41, 58, 1)";
+      navbar.style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.1)";
+      navbar.style.backdropFilter = "blur(7.9px)";
+      navbar.style.webkitBackdropFilter = "blur(7.9px)";
+      navbar.style.border = "1px solid rgba(255, 255, 255, 1)";
+    } else {
+      navbar.style.backgroundColor = "transparent";
+      navbar.style.boxShadow = "";
+      navbar.style.backdropFilter = "";
+      navbar.style.webkitBackdropFilter = "";
+      navbar.style.border = "";
+    }
+  }
+  
+  
